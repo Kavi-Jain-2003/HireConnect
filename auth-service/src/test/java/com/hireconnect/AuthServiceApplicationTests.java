360ecc051ce63b6c79f20109004aa8927bbc994d
Kavi-Jain-2003 <kavijain220803@gmail.com>
2026-04-29 12:46:59 +0530

    final commit of backend

--- auth-service/src/test/java/com/hireconnect/AuthServiceApplicationTests.java
+++ auth-service/src/test/java/com/hireconnect/AuthServiceApplicationTests.java
@@ -3,7 +3,9 @@ package com.hireconnect;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import com.hireconnect.auth.AuthServiceApplication;
+
+@SpringBootTest(classes = AuthServiceApplication.class)
 class AuthServiceApplicationTests {
 
 	@Test
